f4d95f623c0c4bd914cd6578281d20f30efdf568
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-09 13:07:21 -0300

    Componente Form criado

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,10 @@
+import {Container, Input, Button} from "./style";
+
+export default function Form(){
+    return (
+        <Container>
+            <Input />
+            <Button>Search</Button>
+        </Container>
+    )
+}
---
+++ src/components/Form/style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    width: 100%;
+    display: flex;
+    margin: auto;
+    margin-top: 35px;
+`
+
+export const Input = styled.input`
+    display: block;
+    width: 100%;
+    padding: 10px;
+    outline: none;
+    border: none;
+    border: 1px solid #ccc;
+`
+export const Button = styled.button`
+    border: none;
+    outline: none;
+    border: 1px solid #ccc;
+    border-left: none;
+`
