5fee3fbe7a8653d7cab352d2dc40ce56d92eab63
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-09 20:56:38 -0300

    Componente Layout finalizado

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,17 +3,32 @@ import Header from './../Header';
 import DetailsUser from './../DetailsUser';
 import Message from './../Message';
 import Form from './../Form';
+import Table from './../Table';
 
-export default function Layout(){
-    return(
-       <Container>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+export default function Layout() {
+
+    let [users, setUsers] = useState([]);
+
+    useEffect(() => {
+      async function getData() {
+        const result = await axios.get("https://randomuser.me/api/?results=50");
+        setUsers(result.data["results"]);
+      }
+      getData();
+    }, []);
+
+    return (
+        <Container>
             <Header />
-           <DetailsUser />
-           <Main>
+            <DetailsUser />
+            <Main>
                 <Message text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make"} />
                 <Form />
-                {/*<Table /> */}
-           </Main>
-       </Container>
+                <Table data={users} />
+            </Main>
+        </Container>
     )
 }
