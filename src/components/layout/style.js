8946ebfcf239027062ef8718b2c91e8c91e50e06
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-09 13:01:08 -0300

    Atualizando o estilo do componente Layout

--- src/components/layout/style.js
+++ src/components/layout/style.js
@@ -1,7 +1,13 @@
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Main = styled.div`
+  width: 70%;
+  margin: auto;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
