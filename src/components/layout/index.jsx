2ea364e9f6c7a814241545bcc69446b9376b2bac
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-09 12:25:48 -0300

    Criado os componentes Header e Layout, Header ainda em progresso

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,16 @@
+import { Container } from './style'
+import Header from './../Header';
+
+export default function Layout(){
+    return(
+       <Container>
+            <Header />
+           {/*<DetailsUser />
+           <Main>
+                <Description />
+                <Input />
+               <Table />
+           </Main> */}
+       </Container>
+    )
+}
