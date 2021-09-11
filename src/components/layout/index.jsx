d8d183e74cbc648f568d3101f02cbedd0f757c12
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-10 22:44:51 -0300

    Adiconado rotas ao projeto

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,9 +1,10 @@
-import { Container} from './style'
+import { Container } from './style'
 import Header from './../Header';
 import DetailsUser from './../DetailsUser';
 import Message from './../Message';
 import Form from './../Form';
 import Table from './../Table';
+import UserDetails from './../UserDetails';
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -12,27 +13,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container as Main } from 'react-bootstrap';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 export default function Layout() {
 
-    let [users, setUsers] = useState([]);
+  let [users, setUsers] = useState([]);
 
-    useEffect(() => {
-      async function getData() {
-        const result = await axios.get("https://randomuser.me/api/?results=50");
-        setUsers(result.data["results"]);
-      }
-      getData();
-    }, []);
+  useEffect(() => {
+    async function getData() {
+      const result = await axios.get("https://randomuser.me/api/?results=50&seed=foobar");
+      setUsers(result.data["results"]);
+    }
+    getData();
+  }, []);
 
-    return (
-        <Container>
+  return (
+    <Router>
+      <Route path="/" exact render={() => (
+        <>
+          <Container>
             <Header />
             <DetailsUser />
             <Main>
-                <Message text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make"} />
-                <Form />
-                <Table data={users} />
+              <Message text={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make"} />
+              <Form />
+              <Table data={users} />
             </Main>
-        </Container>
-    )
+          </Container>
+        </>
+      )} />
+
+      <Route path="/:uuid" exact component={() => <UserDetails data={users}/>  } />
+    </Router>
+  )
 }
