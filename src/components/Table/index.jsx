a2ce7b78e6df98c747e7d9c23e5574053a322dad
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-11 09:11:40 -0300

    Adicionando o filtro de gênero na tabela

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Button } from './style';
+import { Container, Button, TableRow } from './style';
 import Modal from './../Modal';
 import { formatDate } from './../../utils/utils.js';
 import { useState } from 'react';
@@ -6,6 +6,11 @@ import { Table as TableBootstrap } from 'react-bootstrap';
 
 export default function Table({ data }) {
     const [dataModal, setDataModal] = useState({ active: false, item: null });
+    const [query, setQuery] = useState('-');
+    function filterTable(e) {
+        const { value } = e.target
+        setQuery(value);
+    }
 
     return (
         <Container>
@@ -13,7 +18,13 @@ export default function Table({ data }) {
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Gender</th>
+                        <th>
+                            <select name="" id="" onChange={filterTable}>
+                                <option value="-">Gender</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </th>
                         <th>Birth</th>
                         <th>Actions</th>
                     </tr>
@@ -21,19 +32,19 @@ export default function Table({ data }) {
                 <tbody>
                     {
                         data.map((item, index) => {
-                            return (<tr key={index}>
-                                <th>{item.name.first}</th>
-                                <th>{item.gender}</th>
-                                <th>{formatDate(item.dob.date)}</th>
-                                <th>
-                                    <Button onClick={() => setDataModal({ active: true, item: item })}>Details</Button>
-                                </th>
-                            </tr>)
+                            return (<TableRow key={index} gender={item.gender} filter={query}>
+                                <td>{item.name.first}</td>
+                                <td>{item.gender}</td>
+                                <td>{formatDate(item.dob.date)}</td>
+                                <td>
+                                    <Button teste={'ola'} onClick={() => setDataModal({ active: true, item: item })}>Details</Button>
+                                </td>
+                            </TableRow>)
                         })
                     }
                 </tbody>
             </TableBootstrap>
-            {dataModal.active && <Modal data={dataModal} setDataModal={setDataModal}/>}
+            {dataModal.active && <Modal data={dataModal} setDataModal={setDataModal} />}
         </Container>
     )
 }
