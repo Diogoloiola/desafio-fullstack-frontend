661282d99d287c8da849c24eaef93dae424a8ee2
Diogo Alves <diogomouraloiola@gmail.com>
2021-09-10 22:44:32 -0300

    Criado o componente que mostra as informações de um único usuário

---
+++ src/components/UserDetails/index.jsx
@@ -0,0 +1,21 @@
+import Modal from './../Modal';
+import { useState } from 'react';
+import { useParams } from 'react-router';
+
+function getUser(users, uuid) {
+    if (users !== undefined) {
+        return users.filter(user => user.login.uuid === uuid)[0];
+    }
+    return null;
+}
+
+export default function UserDetails({ data }) {
+    const { uuid } = useParams();
+    const user = getUser(data, uuid);
+    const [dataModal, setDataModal] = useState({ active: true, item: user });
+    return (
+        <>
+            {user && <Modal data={dataModal} setDataModal={setDataModal} />}
+        </>
+    )
+}
